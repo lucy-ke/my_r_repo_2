3f3eb702156fb64f7660afe56e78169c8e83ce2b
lucy-ke <lucyengar@Lucys-MacBook-Pro-3.local>
2025-06-27 10:46:36 -0600

    Added another small amount of code as well as a graph.

--- lmExample.R
+++ lmExample.R
@@ -4,3 +4,11 @@ y = x + rnorm(100, mean = 0, sd = 2)
 fit = lm(y ~ x)
 
 summary(fit)
+
+plot(x,y)
+abline(fit, col=2)
+
+co2 = read.csv("co2_mm_mlo (1).csv")
+plot(co2$decdate, co2$interpolated, type='l', main="Mauna Loa CO2",
+     xlab="Time", ylab="ppm")
+lines(co2$decdate, co2$trend, col=2, lwd=2)
